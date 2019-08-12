c8d3ea3e51d8facc607521ebd03f14b783b4fb0e
Ben Healey <bentastic27@gmail.com>
2019-08-12 11:55:54 -0400

    only pertains to .tar.gz files

--- rancher/logs-collector-unpacker.sh
+++ rancher/logs-collector-unpacker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for file in $(ls); do
+for file in $(ls | grep '.tar.gz'); do
   dirname=$(echo $file | cut -f1 -d. | perl -p -e 's/(.*)\-\d{4}\-\d{2}\-\d{2}_.*/$1/')
   mkdir $dirname;
   tar zxvf $file -C $dirname;
