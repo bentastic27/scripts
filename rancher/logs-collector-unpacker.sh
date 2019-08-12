b0ea7c768a3123ee7abd33360b4c348e7f644044
Ben Healey <bentastic27@gmail.com>
2019-08-12 08:14:51 -0400

    add logs collector unpacker

---
+++ rancher/logs-collector-unpacker.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+for file in $(ls); do
+  dirname=$(echo $file | cut -f1 -d. | perl -p -e 's/(.*)\-\d{4}\-\d{2}\-\d{2}_.*/$1/')
+  mkdir $dirname;
+  tar zxvf $file -C $dirname;
+  mv $file $dirname/
+done
